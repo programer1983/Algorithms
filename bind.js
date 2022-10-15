319be9a9c1729359a879ba8b1fddd190afde250d
programer1983 <programer972@gmail.com>
2022-10-15 19:33:39 +0300

    Custom bind function

---
+++ bind.js
@@ -0,0 +1,26 @@
+// Custom bind function
+
+
+// Implement the `bind` function.
+
+// Input: Object, arguments
+
+// Output: Function
+
+// Function.prototype.myBind = function(context, ...args){
+//     return (...rest) => {
+//         return this.call(context, ...args.concat(rest))
+//     }
+// }
+
+
+// function log(...props){
+//     console.log(this.name, this.age, ...props)
+// }
+
+// const obj = {name: "Kelli", age: 35}
+
+// const bound = log.myBind(obj, 45)
+// bound(15)
+
+
